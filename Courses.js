824f192d8328e40f48b4059cb938412de0120182
Muadmuj35 <148182776+Muadmuj35@users.noreply.github.com>
2023-11-21 00:29:45 +0400

    Add files via upload

---
+++ Courses.js
@@ -0,0 +1,92 @@
+let Courses = [
+	{
+        id: 1,
+        subject: "Computer Science",
+        location: "Manchester",
+        price : 600,
+        image: "images/ComputerScience.jpeg",
+        availablespace: 5,
+        rating: 3
+    },
+    {
+        id: 2,
+        subject: "Math",
+        location: "Edinburgh",
+        price : 400,
+        image: "images/Math.jpeg",
+        availablespace: 5,
+        rating: 4
+    },
+    {
+        id: 3,
+        subject: "Music",
+        location: "Liverpool",
+        price : 250,
+        image: "images/Music.jpeg",
+        availablespace: 5,
+        rating: 5
+    },
+    {
+        id: 4,
+        subject: "Chemistry",
+        location: "Glasgow",
+        price : 200,
+        image: "images/Chemistry.jpeg",
+        availablespace: 5,
+        rating: 5   
+    },
+    {
+        id: 5,
+        subject: "Biology",
+        location: "London",
+        price : 300,
+        image: "images/Biology.jpeg",
+        availablespace: 5,
+        rating: 2   
+    },
+    {
+        id: 6,
+        subject: "History",
+        location: "Stratford",
+        price : 150,
+        image: "images/History.jpeg",
+        availablespace: 5,
+        rating: 3    
+    },
+    {
+        id: 7,
+        subject: "Geography",
+        location: "Cardiff",
+        price : 400,
+        image: "images/Geography.jpeg",
+        availablespace: 5,
+        rating: 4   
+    },
+    {
+        id: 8,
+        subject: "Philosophy",
+        location: "Tottenham",
+        price : 250,
+        image: "images/Philosophy.jpeg",
+        availablespace: 5,
+        rating: 5 
+    },
+    {
+        id: 9,
+        subject: "Engineering",
+        location: "Birmingham",
+        price : 200,
+        image: "images/Engineering.jpeg",
+        availablespace: 5,
+        rating: 3
+    },
+    {
+        id: 10,
+        subject: "Astronomy",
+        location: "Glasgow",
+        price : 300,
+        image: "images/Astronomy.jpeg",
+        availablespace: 5,
+        rating: 4      
+    },
+]
